61706afa26db313d7b95b543713d5e1f7093fdb6
Cassio Pereira <cassiorodp@gmail.com>
2022-02-17 09:24:37 -0300

    initial setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ebytr Lista de Tarefas</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/api/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3001';
+
+export const getTasks = async () => {
+  const tasks = await axios(`${BASE_URL}/tasks`);
+
+  return tasks;
+};
+
+export const createTask = async ({ status, task, createdAt }) => {
+  const createdTask = await axios({
+    method: 'post',
+    url: `${BASE_URL}/tasks`,
+    data: {
+      status,
+      task,
+      createdAt,
+    },
+  });
+
+  return createdTask;
+};
